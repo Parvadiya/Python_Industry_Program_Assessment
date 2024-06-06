50bbd81d59f0d27faed7329cbdb1f067d74fa30c
Parvadiya <145123910+Parvadiya@users.noreply.github.com>
2024-06-06 15:50:00 +0530

    main

---
+++ Organic Veggies & Fruits/myapp/urls.py
@@ -0,0 +1,22 @@
+from django.urls import path
+from .views import *
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('', index, name='index'),
+    path('index/', index, name='index'),
+    path('shop/', shop, name='shop'),
+    path('shop-detail/',shopdetail,name='shop-detail'),
+    path('cart/',cart,name='cart'),
+    path('chackout/',chackout,name='chackout'),
+    path('testimonial/',testimonial,name='testimonial'),
+    path('err/',err,name='err'),
+    path('contactt/', contact, name='contact'),
+    path('login/',loginpage,name="login"),
+    path('registration/',regpage,name="reg"),
+    path("logout/",logoutpage,name="logout")
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
